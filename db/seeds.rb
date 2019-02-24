2097dc76ea285cc17f67754d74375e4366cb287d
kouchou <kouchou@>
2019-02-25 04:16:32 +0900

    [Add] 初期データを入れた

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,45 @@
 5.times do |i|
   Post.create(title: "title #{i}", body: "body#{i}")
 end
+
+Ability.create(name: "リベンジ", description: "戦闘不能になる時、攻撃してきた敵に大きなダメージを与える")
+Ability.create(name: "ガッツ", description: "戦闘不能になる時、一定回数HP1で生き残る")
+
+Property.create(name: "flare")
+Property.create(name: "light")
+
+Leaderskill.create(name: "英帝の威光", description: "ヒューマレルム所属ユニットの攻撃力大アップ")
+Leaderskill.create(name: "軍帝の乱舞", description: "スペアレルム所属のユニットのスピード大アップ")
+
+Realm.create(name: "ヒューマ")
+Realm.create(name: "スペア")
+
+Type.create(name: "アッタッカー")
+Type.create(name: "ヒーロー")
+
+Character.create(
+  name: "ゼクス",
+  realm_id: 1,
+  property_id: 1,
+  rarity: 5,
+  type_id: 1,
+  leaderskill_id: 1,
+  skill: "skill",
+  skill_description: "description",
+  ability1_id: 1,
+  ability2_id: 2,
+  ability3_id: 1
+)
+Character.create(
+  name: "バルザック",
+  realm_id: 2,
+  property_id: 2,
+  rarity: 5,
+  type_id: 2,
+  leaderskill_id: 2,
+  skill: "skill bal",
+  skill_description: "description bal",
+  ability1_id: 1,
+  ability2_id: 1,
+  ability3_id: 2
+)
