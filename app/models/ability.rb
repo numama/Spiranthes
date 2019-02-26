54d7a486192c19cc61d270de573d2a877cffbaf2
kouchou <kouchou@>
2019-02-26 19:14:08 +0900

    [Fix] Abilityモデルからのリレーションを適切に修正

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,5 @@
 class Ability < ApplicationRecord
-  has_many :characters
+  has_many :ability1, class_name: 'Character', :foreign_key => 'ability1_id'
+  has_many :ability2, class_name: 'Character', :foreign_key => 'ability2_id'
+  has_many :ability3, class_name: 'Character', :foreign_key => 'ability3_id'
 end
