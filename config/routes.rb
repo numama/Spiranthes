ecc078cb5ba9822574e4c2fd9d22aad30a6b5358
kouchou <kouchou@>
2019-02-28 08:57:22 +0900

    [Update] 管理者じゃないとユーザを操作できないように変更しルーティングを再生

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
   resources :characters
 
   # ユーザ
-  # get 'signup', to: 'users#new'
-  # post 'users', to: 'users#create'
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
 
   # セッション
   get  'login',  to: 'sessions#new'
