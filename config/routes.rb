777193477ba994c365eb901405e81e7d0818d698
kouchou <kouchou@>
2019-02-28 05:23:33 +0900

    [Add] セッションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # ルート
+  root 'characters#index'
+
+  # チュートリアル
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
 
+  # ユニット
   resources :characters
 
-  root 'characters#index'
+  # セッション
+  get  'login',  to: 'sessions#new'
+  post  'login',  to: 'sessions#create'
+  delete  'logout',  to: 'sessions#destroy'
 
 end
