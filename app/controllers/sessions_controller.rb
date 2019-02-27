749b4023065ef5c280e9b3ba3b05267f2a3f2a13
kouchou <kouchou@>
2019-02-28 07:57:58 +0900

    [Add] ログイン時にトークンをCookieとDBに保存する処理を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,20 +3,19 @@ class SessionsController < ApplicationController
   def new
   end
 
-  # 現状： ユーザ名のレコードがなければ戻す
+  # ユーザ名のレコードがなければ
   # ユーザ名とパスワードがDBと一致していなければ戻す
-  # 一致していればリダイレクト、一致判定のときにすでにSessionが埋め込まれてる・・？
+  # 一致していればsign_inメソッドを実行
   def create
-    if @user = User.find_by(name: session_params[:name])
-      if @user.authenticate(session_params[:password])
-        # sign_in(@user)
-        redirect_to root_path
-      else
-        render 'new'
-      end
+    user = User.find_by(name: session_params[:name])
+    if user && user.authenticate(session_params[:password]) # authenticateで一致検証
+      sign_in(user)
+      redirect_to root_path # 管理画面がないのでとりあえずルートへ
     else
       render 'new'
-    end  
+      # ここにエラー処理書きたい
+      # flashメソッド調べてみる
+    end
   end
 
   def destroy
@@ -29,4 +28,22 @@ class SessionsController < ApplicationController
       params.require(:session).permit(:name, :password)
     end
 
+    def sign_in(user)
+      # Userモデルに書いたトークン生成のメソッド呼び出す
+      remember_token = User.new_remember_token
+      # Cookieにトークンをセット
+      # cookies.permanentで20年クッキーらしい・・なぜ？
+      cookies.permanent[:remember_token] = remember_token
+      # updateメソッドでremember_tokenを書き換える
+      # そのトークンはUserモデルに書いたハッシュ化メソッドでハッシュ化する
+      user.update(remember_token: User.encrypt(remember_token))
+      # @current_userにログインできたユーザをセット
+      # これこのあとリダイレクトしても変数って渡っていくの？　消えるならここでセットする意味ない
+      @current_user = user
+    end
+
+    def sign_out
+      # Cookieからトークンを削除
+      cookies.delete(:remember_token)
+    end
 end
