f34606dddbcd1e160eaaccf98a518d1ea55f0ae5
kouchou <kouchou@>
2019-02-28 18:22:01 +0900

    [Add] 一通りキャラクターコントローラを実装

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -12,22 +12,54 @@ class CharactersController < ApplicationController
   end
 
   def new
-    
+    @character = Character.new
   end
 
   def create
-    
+    @character = Character.new(character_params)
+    if @character.save
+      redirect_to new_character_path
+    else
+      render 'new'
+    end
   end
 
   def edit
-    
+    @character = Character.find(params[:id])
   end
 
   def update
-    
+    @character = Character.find(params[:id])
+    if @character.update(character_params)
+      redirect_to character_path(params[:id])
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-    
+    @character = Character.find(params[:id])
+    @character.destroy
+    redirect_to characters_path
   end
+
+  private
+    def character_params
+      params.require(:character).permit(
+        :name,
+        :realm_id,
+        :property_id,
+        :rarity,
+        :type_id,
+        :leaderskill_id,
+        :skill,
+        :skill_description,
+        :ability1_id,
+        :ability2_id,
+        :ability3_id,
+        :guild_battle_score,
+        :rolling_quest_score,
+        :body
+      )
+    end
 end
