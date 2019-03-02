4e5b3fbbe728469483df95c96eddc9c24acff3c3
kouchou <kouchou@>
2019-03-03 07:19:40 +0900

    [Add] コメントのバリデーションをかいてみた

--- app/models/character_comment.rb
+++ app/models/character_comment.rb
@@ -1,2 +1,11 @@
 class CharacterComment < ApplicationRecord
+  validates :name,
+    lnegth: { in: 1..30, allow_nil: true, message: "名前は30文字以内で指定してください" }
+  validates :score,
+    lnegth: { in: 1..3, allow_nil: true },
+    numericality: true
+  validates :body,
+    presence: { true, message: "コメント本文の入力は必須です" },
+    length: { minimum: 5, message: "5文字以上記述してください" }
+    length: { maximam: 1000, message: "1000文字以内で記述してください" }
 end
