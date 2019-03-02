0ac261aee9c5f4becdf81d4d56d2022896543005
kouchou <kouchou@>
2019-03-03 02:24:00 +0900

    [Add] リレーションを張ったカラムの初期値を設定

--- db/migrate/20190302154346_create_characters.rb
+++ db/migrate/20190302154346_create_characters.rb
@@ -3,16 +3,16 @@ class CreateCharacters < ActiveRecord::Migration[5.2]
     create_table :characters do |t|
       t.string :name
       t.string :symbol
-      t.references :realm
-      t.references :property
+      t.references :realm, default: 1
+      t.references :property, default: 1
       t.integer :rarity
-      t.references :type
-      t.references :leaderskill
+      t.references :type, default: 1
+      t.references :leaderskill, default: 1
       t.string  :skill
       t.text    :skill_description
-      t.integer :ability1_id
-      t.integer :ability2_id
-      t.integer :ability3_id
+      t.integer :ability1_id, default: 1
+      t.integer :ability2_id, default: 1
+      t.integer :ability3_id, default: 1
       t.integer :guild_battle_score
       t.integer :rolling_quest_score
       t.text    :body
