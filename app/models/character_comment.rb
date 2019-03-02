eb558838927c7da78c50223a48f51433fd12187b
kouchou <kouchou@>
2019-03-03 08:03:40 +0900

    [Fix] バリデーションを修正

--- app/models/character_comment.rb
+++ app/models/character_comment.rb
@@ -1,11 +1,13 @@
 class CharacterComment < ApplicationRecord
+  validates :character_id,
+    presence: { message: "不正な操作です" }
   validates :name,
-    lnegth: { in: 1..30, allow_nil: true, message: "名前は30文字以内で指定してください" }
+    allow_nil: true,
+    length: { in: 1..30, message: "名前は30文字以内で指定してください" }
   validates :score,
-    lnegth: { in: 1..3, allow_nil: true },
-    numericality: true
+    allow_nil: true,
+    length: { in: 1..3 },
+    numericality: { message: "不正な操作です" }
   validates :body,
-    presence: { true, message: "コメント本文の入力は必須です" },
-    length: { minimum: 5, message: "5文字以上記述してください" }
-    length: { maximam: 1000, message: "1000文字以内で記述してください" }
+    length: { in: 5..1000, message: "本文は5~1000文字で記述してください" }
 end
