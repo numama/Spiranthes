274ad5e816e4f02236fe4add322d2c4b62c1c9da
kouchou <kouchou@>
2019-03-07 18:29:02 +0900

    [Fix] リレーションのミスを修正

--- app/models/character.rb
+++ app/models/character.rb
@@ -5,7 +5,7 @@ class Character < ApplicationRecord
   belongs_to :type
   belongs_to :special_leaderskill, optional: true
   belongs_to :head_leaderskill, optional: true
-  belongs_to :head_leaderskill, optional: true
+  belongs_to :foot_leaderskill, optional: true
   belongs_to :ability1, class_name: 'Ability', :foreign_key => 'ability1_id', optional: true
   belongs_to :ability2, class_name: 'Ability', :foreign_key => 'ability2_id', optional: true
   belongs_to :ability3, class_name: 'Ability', :foreign_key => 'ability3_id', optional: true
