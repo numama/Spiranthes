fea5288a97b6f2590eb6910aa830464438f05d92
kouchou <kouchou@>
2019-03-07 18:14:22 +0900

    [Update] マイグレーションファイル変更、カラムの追加と初期値の設定

--- db/migrate/20190302154346_create_characters.rb
+++ db/migrate/20190302154346_create_characters.rb
@@ -5,14 +5,16 @@ class CreateCharacters < ActiveRecord::Migration[5.2]
       t.string :symbol
       t.references :realm, default: 1
       t.references :property, default: 1
-      t.integer :rarity
+      t.integer :rarity, default: 5
       t.references :type, default: 1
-      t.references :leaderskill, default: 1
+      t.references :special_leaderskill
+      t.references :head_leaderskill
+      t.references :foot_leaderskill
       t.string  :skill
       t.text    :skill_description
-      t.integer :ability1_id, default: 1
-      t.integer :ability2_id, default: 1
-      t.integer :ability3_id, default: 1
+      t.integer :ability1_id
+      t.integer :ability2_id
+      t.integer :ability3_id
       t.integer :guild_battle_score
       t.integer :rolling_quest_score
       t.text    :body
