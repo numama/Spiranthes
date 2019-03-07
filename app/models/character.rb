2d0bfdd1aa33feba0b549af2b4a160e293024df1
kouchou <kouchou@>
2019-03-07 18:13:01 +0900

    [Update] belongs_toだけどnullを許可に（optional: true)

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,10 +3,12 @@ class Character < ApplicationRecord
   belongs_to :realm
   belongs_to :property
   belongs_to :type
-  belongs_to :leaderskill
-  belongs_to :ability1, class_name: 'Ability', :foreign_key => 'ability1_id'
-  belongs_to :ability2, class_name: 'Ability', :foreign_key => 'ability2_id'
-  belongs_to :ability3, class_name: 'Ability', :foreign_key => 'ability3_id'
+  belongs_to :special_leaderskill, optional: true
+  belongs_to :head_leaderskill, optional: true
+  belongs_to :head_leaderskill, optional: true
+  belongs_to :ability1, class_name: 'Ability', :foreign_key => 'ability1_id', optional: true
+  belongs_to :ability2, class_name: 'Ability', :foreign_key => 'ability2_id', optional: true
+  belongs_to :ability3, class_name: 'Ability', :foreign_key => 'ability3_id', optional: true
   has_one :character_status
 
   # validates :name,
