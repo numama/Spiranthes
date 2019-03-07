d82787e0d988d59f8d494eb798de86d793ad81a8
kouchou <kouchou@>
2019-03-07 20:07:52 +0900

    [Update] リーダースキルの頭と足それぞれの配列を取得するように変更

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -13,14 +13,16 @@ class CharactersController < ApplicationController
 
   def new
     @character = Character.new
-    @leaderskill_array = Leaderskill.get_array
+    @head_leaderskill_array = HeadLeaderskill.get_array
+    @foot_leaderskill_array = FootLeaderskill.get_array
     @ability_array = Ability.get_array
   end
 
   def create
     @character = Character.new(character_params)
     # なんか以下2つを追加しないとエラーになる
-    @leaderskill_array = Leaderskill.get_array
+    @head_leaderskill_array = HeadLeaderskill.get_array
+    @foot_leaderskill_array = FootLeaderskill.get_array
     @ability_array = Ability.get_array
     if @character.save
       redirect_to character_path(@character)
@@ -31,13 +33,15 @@ class CharactersController < ApplicationController
 
   def edit
     @character = Character.find(params[:id])
-    @leaderskill_array = Leaderskill.get_array
+    @head_leaderskill_array = HeadLeaderskill.get_array
+    @foot_leaderskill_array = FootLeaderskill.get_array
     @ability_array = Ability.get_array
   end
 
   def update
     @character = Character.find(params[:id])
-    @leaderskill_array = Leaderskill.get_array
+    @head_leaderskill_array = HeadLeaderskill.get_array
+    @foot_leaderskill_array = FootLeaderskill.get_array
     @ability_array = Ability.get_array
     if @character.update(character_params)
       redirect_to character_path(params[:id])
