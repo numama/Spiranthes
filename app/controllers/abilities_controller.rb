3b95bbdba594651a844e744208886d21821fccfd
kouchou <kouchou@>
2019-03-11 00:18:54 +0900

    [Add] 一覧取得時にはイーガーロードするように

--- app/controllers/abilities_controller.rb
+++ app/controllers/abilities_controller.rb
@@ -3,6 +3,6 @@ class AbilitiesController < ApplicationController
     @ability = Ability.find(params[:id])
     @characters = @ability.characters.select(
       :id, :name, :rarity, :property_id, :realm_id, :type_id, :rolling_quest_score, :guild_battle_score
-    )
+    ).includes(:property, :realm)
   end
 end
