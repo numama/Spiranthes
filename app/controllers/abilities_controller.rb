cb0ab50121e56bb5eb79c271664a502d52773e19
kouchou <kouchou@>
2019-03-11 00:34:33 +0900

    [Update] ぜったい１つなのでfind_byを使ってみた

--- app/controllers/abilities_controller.rb
+++ app/controllers/abilities_controller.rb
@@ -1,6 +1,6 @@
 class AbilitiesController < ApplicationController
   def show
-    @ability = Ability.find(params[:id])
+    @ability = Ability.find_by(params[:id])
     @characters = @ability.characters.select(
       :id, :name, :rarity, :property_id, :realm_id, :type_id, :rolling_quest_score, :guild_battle_score
     ).includes(:property, :realm)
