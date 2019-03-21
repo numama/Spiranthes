7b5ac9f621ae31f70a2578f5eb1450071e85e5b5
kouchou <kouchou@>
2019-03-22 00:04:29 +0900

    [Add] 現在のページを取得して見た目を変える処理を実装

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,15 +1,28 @@
-// イベントリスナーでDOMがロードされたあとに実行する
-addEventListener('DOMContentLoaded', function() {
-  // ナビゲーションのエレメントを取得
-  var gnav = document.getElementById("gnav");
+// // イベントリスナーでDOMがロードされたあとに実行する
+// addEventListener('DOMContentLoaded', function() {
+//   // ナビゲーションのエレメントを取得
+//   var gnav = document.getElementById("gnav");
 
-  var path = location.pathname;
+//   var path = location.pathname;
 
-  // gnavは実はイテレータではないのでそのままではforEach使えない
-  // Array.from()で配列にしてから繰り返す
-  Array.from(gnav.children).forEach(function(tag) {
-    if (tag.getAttribute("href") === path) {
-      tag.classList.add("current");
-    }
-  });
+//   // gnavは実はイテレータではないのでそのままではforEach使えない
+//   // Array.from()で配列にしてから繰り返す
+//   Array.from(gnav.children).forEach(function(tag) {
+//     if (tag.getAttribute("href") === path) {
+//       tag.classList.add("current");
+//     }
+//   });
+// });
+
+// ナビゲーションのエレメントを取得
+var gnav = document.getElementById("gnav");
+
+var path = location.pathname;
+
+// gnavは実はイテレータではないのでそのままではforEach使えない
+// Array.from()で配列にしてから繰り返す
+Array.from(gnav.children).forEach(function(tag) {
+  if (tag.getAttribute("href") === path) {
+    tag.classList.add("current");
+  }
 });
